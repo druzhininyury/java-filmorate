bf871ac9a6af842b8b9b71f906227de53a2598d3
Druzhinin Yury <druzhinin.yy@gmail.com>
2023-08-23 00:24:38 +0300

    fix: codestyle.

--- src/test/java/ru/yandex/practicum/filmorate/FilmControllerTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/FilmControllerTest.java
@@ -13,9 +13,9 @@ public class FilmControllerTest {
     @Test
     public void filmValidationTest() {
         Film film;
-        film = new Film( "Name", "Description", LocalDate.of(1870, 1, 1), 90);
+        film = new Film("Name", "Description", LocalDate.of(1870, 1, 1), 90);
         assertFalse(FilmController.isFilmValid(film));
-        film = new Film( "Name", "Description", LocalDate.of(1895, 12, 28), 90);
+        film = new Film("Name", "Description", LocalDate.of(1895, 12, 28), 90);
         assertTrue(FilmController.isFilmValid(film));
     }
 }
